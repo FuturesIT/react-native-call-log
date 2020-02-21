e6bc82e9db7ab12120d7fade8f8e13b471954ac5
Thuong Nguyen <thuong.nguyen@alphatechnologies.net>
2020-02-21 13:43:56 +0700

    Update example project

---
+++ Example/ios/Pods/Target Support Files/Pods-CallLog/Pods-CallLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CallLog : NSObject
+@end
+@implementation PodsDummy_Pods_CallLog
+@end
---
+++ Example/ios/Pods/Target Support Files/Pods-CallLogTests/Pods-CallLogTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CallLogTests : NSObject
+@end
+@implementation PodsDummy_Pods_CallLogTests
+@end
